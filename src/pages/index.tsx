3ff8bb347cfb34fea96f0e04560da56777db8e56
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-09-23 18:38:17 +0800

    feat: all demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,46 @@
-import { useTZ } from "../context";
+import React from "react";
+import { Button, Card, Flex, Skeleton } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+const demos = import.meta.glob<{ default: React.ComponentType }>('./!(index).tsx');
 
 function Index() {
-  const tz = useTZ();
+  const renderDemos = Object.entries(demos)
+    .map(([key, load]) => ({
+      Component: React.lazy(load),
+      key,
+      path: key.replace('./', '').replace('.tsx', ''),
+    }))
+
+  const navigate = useNavigate();
 
   return (
-    <>
-      <h1 >Hello, Vite!</h1>
-      <code >src/pages/index.tsx</code>
-      <p >Timezone: {tz}</p>
-    </>
+    <React.Suspense fallback={<Skeleton active />}>
+      <Flex vertical style={{ padding: 12 }} gap="middle">
+        {
+          renderDemos.map(({ Component, key, path }) => (
+            <Card
+              key={key}
+              title={key}
+              extra={
+                <Button
+                  type="link"
+                  onClick={() => {
+                    React.startTransition(() => {
+                      navigate(path)
+                    });
+                  }}
+                >
+                  View
+                </Button>
+              }
+            >
+              <Component />
+            </Card>
+          ))
+        }
+      </Flex>
+    </React.Suspense>
   );
 }
 
